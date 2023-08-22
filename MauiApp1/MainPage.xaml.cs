990813f566f0219cfcee698f28d20475e46cdd64
Outofnothing <chriszhou0628@gmail.com>
2023-08-22 23:14:40 +0800

    init

---
+++ MauiApp1/MainPage.xaml.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.ObjectModel;
+
+namespace MauiApp1
+{
+    public class ViewModel 
+    {
+        public ObservableCollection<Type> Types { get; set; } = new()
+        {
+            typeof(string), typeof(int), typeof(float)
+        };
+    }
+
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+            BindingContext = new ViewModel();
+        }
+    }
+}
